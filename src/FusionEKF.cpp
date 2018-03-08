4818aacdcd3e5bbf91b49cc550f8f5a314dd1579
Genzaiwuxian <31791192+Genzaiwuxian@users.noreply.github.com>
2018-03-08 21:58:23 +0800

    new

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -38,9 +38,6 @@ FusionEKF::FusionEKF() {
   */
   H_laser_ << 1, 0, 0, 0,
 	  0, 1, 0, 0;
-  Hj_ << 1, 1, 1, 1,
-	  1, 1, 1, 1,
-	  1, 1, 1, 1;
 
 }
 
@@ -139,7 +136,8 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
 
   if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
     // Radar updates
-	  ekf_.H_ = tools.CalculateJacobian(ekf_.x_);
+	  Hj_= tools.CalculateJacobian(ekf_.x_);
+	  ekf_.H_ = Hj_;
 	  ekf_.R_ = R_radar_;
 	  ekf_.UpdateEKF(measurement_pack.raw_measurements_);
   } else {
