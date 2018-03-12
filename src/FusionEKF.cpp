80b2d72c79723e1351b09daf2b7d577447493a5e
Genzaiwuxian <31791192+Genzaiwuxian@users.noreply.github.com>
2018-03-12 20:27:43 +0800

    close the lindar channel

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -146,11 +146,11 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
 	  ekf_.H_ = Hj_;
 	  ekf_.R_ = R_radar_;
 	  ekf_.UpdateEKF(measurement_pack.raw_measurements_);
-  } else {
+  } /*else {
     // Laser updates
 	  ekf_.H_ = H_laser_;
 	  ekf_.R_ = R_laser_;
-	  ekf_.Update(measurement_pack.raw_measurements_);
+	  ekf_.Update(measurement_pack.raw_measurements_);*/
   }
 
   // print the output
